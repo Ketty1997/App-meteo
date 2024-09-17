08b17851a8a4e1e5708d47c3a9fc53ac80b6bfd4
katia <katiafalletti97@gmail.com>
2024-09-17 15:55:50 +0200

    app meteo

---
+++ README.md
@@ -0,0 +1,38 @@
+# App meteo
+
+Questo progetto è un'applicazione del meteo
+
+## Descrizione
+
+Questo progetto è realizzato attraverso l'utilizzo di diverse tecnologie quali html, css ,javascript e webAPI
+
+
+![Pagina calcolo](images/preview/calcolo.png)
+![Pagina citta uno](images/preview/foto1.png)
+![Pagina citta due](images/preview/foto2.png)
+
+
+## Funzionalità
+
+La web app include le seguenti funzionalità:
+
+- Descrizioni e icone che cambiano in base al meteo della citta' in cui ti trovi
+
+
+## Tecnologie utilizzate
+
+la web app è stata sviluppata utilizzando le seguenti tecnologie:
+
+- HTML
+- CSS
+- JavaScript
+
+
+
+## Licenza
+
+<!-- Questo progetto è stato rilasciato sotto la licenza MIT. Per ulteriori informazioni, leggere il file `LICENSE.md`. -->
+
+## Crediti
+
+Questo progetto è stato sviluppato da Katia Falletti.(https://it.linkedin.com/in/katia-falletti-616890225?trk=people-guest_people_search-card)
---
+++ script.js
@@ -0,0 +1,56 @@
+//recupero gli elementi di interesse 
+const htmlElement = document.documentElement;
+const suggestion = document.querySelector('.suggestion');
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherLocation = document.querySelector('.weather-location');
+const weatherTemperature = document.querySelector('.weather-temperature');
+
+//recupero la mia posizione
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+//funzione da eseguire in caso di errore
+function onError() {
+    //preparo degli elementi in pagina per far capire che va attivata
+    weatherLocation.innerText = '';
+    weatherIcon.alt = "geolocation disattivata";
+    weatherIcon.src = "images/geolocation_disabled.png";
+    suggestion.innerText = 'Attiva la geolocalizzazione';
+
+    //disattivo il loading
+    htmlElement.className = '';
+}
+
+//funzione da eseguire in caso di successo
+async function onSuccess(position) {
+    //recupero latitudine e longitudine
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    //prepariamoci a chiamare l'Api di open weather
+    const API_KEY = 'dfde5139a01417a92e128a0dcbd00ab2';
+    const units = 'metric';
+    const lang = 'it'
+
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${units}&lang=${lang}`;
+
+    //chiamo api open weather
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const iconCode = data.weather[0].icon;
+    const description = data.weather[0].description;
+
+    //riempio gli elementi della pagina
+    weatherLocation.innerText = data.name;
+    weatherIcon.alt = description;
+    weatherIcon.src = `images/${iconCode}.png`;
+    weatherTemperature.innerText = `${Math.floor(data.main.temp)}°`;
+    suggestion.innerText = getSuggestion(iconCode);
+
+    //disattivo il loading
+    htmlElement.className = '';
+}
+
+//funzione per decidere il suggerimento appropriato
+function getSuggestion(iconCode) {
+    return suggestions[iconCode];
+}
